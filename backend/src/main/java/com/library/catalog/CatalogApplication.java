e035a0260de9dbee37e4b903de822b02d4e0a17e
Celio Camara <celiofcjunior_cg@hotmail.com>
2023-09-30 03:41:53 -0300

    modified library name typo

--- backend/src/main/java/com/library/catalog/CatalogApplication.java
+++ backend/src/main/java/com/library/catalog/CatalogApplication.java
@@ -1,4 +1,4 @@
-package com.libary.catalog;
+package com.library.catalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/com/library/catalog/CatalogApplicationTests.java
+++ backend/src/test/java/com/library/catalog/CatalogApplicationTests.java
@@ -1,4 +1,4 @@
-package com.libary.catalog;
+package com.library.catalog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
