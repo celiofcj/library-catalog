3e70bb682a58a8c1a1eb98a52be6d9a3b4ef8d55
Celio Camara <celiofcjunior_cg@hotmail.com>
2023-11-02 16:13:32 -0300

    remoeve unnecessary repository annotation in repositories

--- backend/src/main/java/com/library/catalog/repositories/ThemeRepository.java
+++ backend/src/main/java/com/library/catalog/repositories/ThemeRepository.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
-@Repository
 public interface ThemeRepository extends JpaRepository<Theme, Integer> {
     List<Theme> findAllByBooksId(Integer id);
     Optional<Theme> findByName(String name);
--- backend/src/main/java/com/library/catalog/repositories/WriterRepository.java
+++ backend/src/main/java/com/library/catalog/repositories/WriterRepository.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface WriterRepository extends JpaRepository<Writer, Integer> {
     List<Writer> findAllByBooksId(Integer id);
 }
