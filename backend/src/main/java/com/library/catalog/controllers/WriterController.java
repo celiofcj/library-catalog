6dc6b85a734d2d2a076438842459e82221ce8d92
Celio Camara <celiofcjunior_cg@hotmail.com>
2023-11-18 16:34:53 -0300

    Complete CRUD for Writer

---
+++ backend/src/main/java/com/library/catalog/controllers/WriterController.java
@@ -0,0 +1,64 @@
+package com.library.catalog.controllers;
+
+import com.library.catalog.models.Writer;
+import com.library.catalog.services.WriterService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RequestMapping("/writer")
+@RestController
+public class WriterController {
+    private WriterService writerService;
+
+    public WriterController(WriterService writerService){
+        this.writerService = writerService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Writer> findById(@PathVariable Long id){
+        Writer writer = writerService.findById(id);
+
+        return ResponseEntity.ok(writer);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Writer>> findAll(){
+        List<Writer> writers = writerService.findAll();
+
+        return ResponseEntity.ok(writers);
+    }
+
+    @GetMapping("/book/{bookId}")
+    public ResponseEntity<List<Writer>> findAllByBookId(@PathVariable Long bookId){
+        List<Writer> writers = writerService.findAllByBookId(bookId);
+
+        return ResponseEntity.ok(writers);
+    }
+
+    @PostMapping
+    public ResponseEntity<Writer> create(@RequestBody Writer writer){
+        Writer newWriter = writerService.create(writer);
+        URI uri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/{id}").buildAndExpand(newWriter.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(newWriter);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Writer> update(@PathVariable Long id, @RequestBody Writer writer){
+        Writer updateWriter = writerService.update(id, writer);
+
+        return ResponseEntity.ok(updateWriter);
+    }
+
+    @DeleteMapping("/id")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        writerService.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
+}
--- backend/src/main/java/com/library/catalog/services/WriterService.java
+++ backend/src/main/java/com/library/catalog/services/WriterService.java
@@ -2,10 +2,12 @@ package com.library.catalog.services;
 
 import com.library.catalog.models.Writer;
 import com.library.catalog.repositories.WriterRepository;
-import jakarta.persistence.EntityNotFoundException;
+import com.library.catalog.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,10 +19,46 @@ public class WriterService {
         this.writerRepository = writerRepository;
     }
 
+    @Transactional(readOnly = true)
     public Writer findById(Long id){
         Optional<Writer> writer = writerRepository.findById(id);
 
-        return writer.orElseThrow(() -> new EntityNotFoundException("Not found writer with id: {" +
+        return writer.orElseThrow(() -> new ObjectNotFoundException("Not found writer with id: {" +
                 id + "}."));
+    }
+
+    @Transactional(readOnly = true)
+    public List<Writer> findAll(){
+        return writerRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public List<Writer> findAllByBookId(Long id){
+        return writerRepository.findAllByBooksId(id);
+    }
+
+    @Transactional
+    public Writer create(Writer writer){
+        Writer newWriter = new Writer();
+
+        newWriter.setName(writer.getName());
+        newWriter.setBio(writer.getBio());
+
+        return newWriter;
+    }
+
+    @Transactional
+    public Writer update(Long id, Writer writer){
+        Writer savedWriter = findById(id);
+
+        savedWriter.setName(writer.getName());
+        savedWriter.setBio(writer.getBio());
+
+        return savedWriter;
+    }
+
+    @Transactional
+    public void delete(Long id){
+        writerRepository.deleteById(id);
     }
 }
